3cee53d9644900cde2e5d003042852e8845917d9
Agis24 <agisbetsis@gmail.com>
2025-10-23 21:07:55 +0300

    Guthub pages fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const { i18n } = require('./next-i18next.config');
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['IP'],
+  images: { unoptimized: true },
+  basePath: '/ae-project',
+  assetPrefix: '/ae-project/',
+  trailingSlash: true
 };
 
 module.exports = { nextConfig, i18n };
