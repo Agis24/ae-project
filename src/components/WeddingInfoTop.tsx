682513b552f0b7b8443311926dbcde5ee0850b3f
Agis24 <agisbetsis@gmail.com>
2025-10-23 21:50:03 +0300

    fix #8

--- src/components/WeddingInfoTop.tsx
+++ src/components/WeddingInfoTop.tsx
@@ -1,14 +1,15 @@
-import { withPrefix } from "@/lib/prefix";
+import Image from "next/image";
 
 export default function WeddingInfoTop() {
   return (
     <section className="text-center py-8">
-      <img
-        src={withPrefix('/Aristotelis_and_Elisavet.png')}
+      <Image
+        src="/Aristotelis_and_Elisavet.png"
         alt="Aristotelis and Elisavet"
         width={800}
         height={300}
         className="mx-auto h-auto w-[90%] md:w-[70%] lg:w-[60%]"
+        priority
       />
     </section>
   );
