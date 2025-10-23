4a4d16340e8c99dbba20273e2047715a358ddab2
Agis24 <agisbetsis@gmail.com>
2025-10-23 21:23:20 +0300

    Github pages fix 3

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from 'next';
 
 const { i18n } = require('./next-i18next.config');
 
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['IP'],
+
+const nextConfig = {
+  output: 'export',
   images: { unoptimized: true },
   basePath: '/ae-project',
-  assetPrefix: '/ae-project/',
-  trailingSlash: true
-};
+  assetPrefix: '/ae-project/',   // keep trailing slash
+  trailingSlash: true,
+} satisfies NextConfig;
 
-module.exports = { nextConfig, i18n };
 export default nextConfig;
